3a29914430c8544d4e59e3322cd8af7b6521b6fd
eujap <jose.ap2008@hotmail.com>
2025-12-15 15:37:41 -0400

    "retornando aos estudos"

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,7 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "aprendendo, são " });
+  response.status(200).json({ chave: "espero que agora deu" });
 }
 
 export default status;
