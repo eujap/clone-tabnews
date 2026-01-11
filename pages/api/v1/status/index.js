f34bff6c9887c79a326a7901b624f475a59d3e82
eujap <jose.ap2008@hotmail.com>
2026-01-11 19:36:36 -0400

    adds migrations  scripts

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,6 +3,7 @@ import database from "infra/database.js";
 async function status(request, response) {
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  consolelog(databaseVersionValue);
 
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
